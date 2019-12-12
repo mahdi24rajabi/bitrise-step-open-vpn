24468913efd46057904f5acb8432fa6231beb8b8
mahdirajabi-nent <54574624+mahdirajabi-nent@users.noreply.github.com>
2019-12-12 13:09:46 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -31,12 +31,12 @@ EOF
     echo "Configuring for Ubuntu 16.4"
     
     echo $(base64 -d $ca_crt) >> ca.crt
-    cat /etc/openvpn/client.conf
+    cat ca.crt
     echo "hello"
-     echo $client_crt | base64 -d -o client.crt > /dev/null 2>&1
-     echo $client_key | base64 -d -o client.key > /dev/null 2>&1
-     echo $user_pass | base64 -d -o user-pass > /dev/null 2>&1
-     echo $tls_auth | base64 -d -o tls-auth.key > /dev/null 2>&1
+     #echo $client_crt | base64 -d -o client.crt > /dev/null 2>&1
+     #echo $client_key | base64 -d -o client.key > /dev/null 2>&1
+     #echo $user_pass | base64 -d -o user-pass > /dev/null 2>&1
+     #echo $tls_auth | base64 -d -o tls-auth.key > /dev/null 2>&1
 
 #     sudo openvpn --client --dev tun --proto ${proto} --remote ${host} ${port} --resolv-retry infinite --nobind --persist-key --persist-tun --ca ca.crt --cert client.crt --key client.key --ns-cert-type server --comp-lzo --verb 3 --auth-user-pass user-pass --tls-auth tls-auth.key 1 > /dev/null 2>&1 &
 
