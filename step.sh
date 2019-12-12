a7ed0375c83985482d2d95b1bdcd7c2ff83dafc2
mahdirajabi-nent <54574624+mahdirajabi-nent@users.noreply.github.com>
2019-12-12 14:25:07 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -8,6 +8,8 @@ case "$OSTYPE" in
     echo ${ca_crt} | base64 -d > /etc/openvpn/ca.crt
     echo ${client_crt} | base64 -d > /etc/openvpn/client.crt
     echo ${client_key} | base64 -d > /etc/openvpn/client.key
+    echo ${user_pass} | base64 -d > /etc/openvpn/user-pass
+    echo ${tls_auth} | base64 -d > /etc/openvpn/tls-auth.key
 
     cat <<EOF > /etc/openvpn/client.conf
 client
@@ -27,18 +29,8 @@ EOF
     service openvpn start
     service openvpn status
     sleep 5
-    
-    echo "Configuring for Ubuntu 16.4"
 
-    echo ${user_pass} | base64 -d > user-pass
-    echo ${tls_auth} | base64 -d > tls-auth.key
-    echo ${ca_crt} | base64 -d > ca.crt
-    echo ${client_crt} | base64 -d > client.crt
-    echo ${client_key} | base64 -d > client.key
-    
-    cat tls-auth.key
-
-    openvpn --client --dev tun --proto ${proto} --remote ${host} ${port} --resolv-retry infinite --nobind --persist-key --persist-tun --ca ca.crt --cert client.crt --key client.key --ns-cert-type server --comp-lzo --verb 3 --auth-user-pass user-pass --tls-auth tls-auth.key
+    openvpn --client --dev tun --proto ${proto} --remote ${host} ${port} --resolv-retry infinite --nobind --persist-key --persist-tun --ca /etc/openvpn/ca.crt --cert /etc/openvpn/client.crt --key /etc/openvpn/client.key --ns-cert-type server --comp-lzo --verb 3 --auth-user-pass /etc/openvpn/user-pass --tls-auth /etc/openvpn/tls-auth.key
   ;;  
 *)
 ;;
