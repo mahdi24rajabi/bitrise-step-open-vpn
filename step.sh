87320c00a650e7c5c76c94bf985d3b359deee317
mahdirajabi-nent <54574624+mahdirajabi-nent@users.noreply.github.com>
2019-12-13 13:05:48 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -5,13 +5,13 @@ case "$OSTYPE" in
   linux*)
     echo "Configuring for Ubuntu"
 
-    echo ${ca_crt} | base64 -d > ca.crt
-    echo ${client_crt} | base64 -d > client.crt
-    echo ${client_key} | base64 -d > client.key
-    echo ${user_pass} | base64 -d > user-pass
-    echo ${tls_auth} | base64 -d > tls-auth.key
+    echo ${ca_crt} | base64 -d > /etc/openvpn/ca.crt
+    echo ${client_crt} | base64 -d > /etc/openvpn/client.crt
+    echo ${client_key} | base64 -d > /etc/openvpn/client.key
+    echo ${user_pass} | base64 -d > /etc/openvpn/user-pass
+    echo ${tls_auth} | base64 -d > /etc/openvpn/tls-auth.key
 
-    cat <<EOF > client.ovpn
+    cat <<EOF > /etc/openvpn/client.ovpn
 client
 dev tun
 proto ${proto}
@@ -24,15 +24,15 @@ ns-cert-type server
 comp-lzo
 verb 3
 ca ca.crt
-cert client.crt
-key client.key
-auth-user-pass user-pass
-tls-auth tls-auth.key 1
+cert /etc/openvpn/client.crt
+key /etc/openvpn/client.key
+auth-user-pass /etc/openvpn/user-pass
+tls-auth /etc/openvpn/tls-auth.key 1
 EOF
     service openvpn start
     service openvpn status
     
-    sudo openvpn client.ovpn 1> /dev/null 2>&1 &
+    sudo openvpn /etc/openvpn/client.ovpn 1> /dev/null 2>&1 &
     echo "finished"
   ;;  
 *)
